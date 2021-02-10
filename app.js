27e2412672f64ddbd1389fc1790c4a85c30be764
Asif Bin Latif <asif.655@gmail.com>
2021-02-10 16:54:49 +0600

    share toggle function added

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+//selector
 const shareButton = document.getElementById('shareButton');
 const shareToggle = document.getElementById('shareToggle');
-// window.addEventListener('click', function() {
-//     alert("ASD");
-// });
 
-window.addEventListener('click', (e) => {
+//event listener
+window.addEventListener('click', getClickEvent);
 
-    if (e.target === null || (e.target != shareButton && e.target.parentElement != shareButton && e.target.parentElement.parentElement != shareButton)) {
-        shareToggle.classList.add('hidden');
-        shareButton.classList.remove('active');
-    } else {
+//function
+function getClickEvent(e) {
+
+    const item = e.target;
+    if (item.classList[0] === 'card-share-button') {
         shareToggle.classList.toggle('hidden');
-        shareButton.classList.toggle('active');
-    }
 
-});
+    } else {
+        shareToggle.classList.add('hidden');
+    }
+    // if (e.target === null || (e.target != shareButton && e.target.parentElement != shareButton && e.target.parentElement.parentElement != shareButton)) {
+    //     shareToggle.classList.add('hidden');
+    //     shareButton.classList.remove('active');
+    // } else {
+    //     shareToggle.classList.toggle('hidden');
+    //     shareButton.classList.toggle('active');
+    // }
+
+}
